70a2a8e9c91f2982689bf4639455d4c18263e999
E121977 <kevin.bradbury@asu.edu>
2015-04-26 15:47:03 -0700

    second commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,9 @@
+## Creates an object, contains a matrix data structure and its inverse
+##  get = gets the value of the matrix
+##  set = changes the value of the matrix
+##  getInverse = gets the cached inverse of the matrix once set
+##  setInverse = sets the cached inverse of the matrix
+
 makeCacheMatrix <- function(x = matrix()) {
   Im <- NULL
   set <- function(y) {
@@ -5,12 +11,15 @@ makeCacheMatrix <- function(x = matrix()) {
     Im <<- NULL
   }
   get <- function() x
-  setinverse <- function(inverse) Im <<- inverse
-  getinverse <- function() Im
-  list(set = set, get = get,setinverse = setinverse,getinverse = getinverse)
+  setInverse <- function(solve) Im <<- solve
+  getInverse <- function() Im
+  list(set = set, get = get,setInverse = setInverse,getInverse = getInverse)
 }
 
-cacheSolve <- function(x, ...) {
+## returns the cached inverse value of cachematrix if available
+## otherwise calculates, caches, and returns the inverse of the cachematrix
+
+cacheSolve <- function(x=matrix(), ...) {
   Im <- x$getInverse()
   if(!is.null(Im)) {
     message("getting cached data")
